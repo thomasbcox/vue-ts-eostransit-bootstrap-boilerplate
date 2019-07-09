293a3b00a0d0efdf21cca3db4ef6ec1b96dfb494
Jan Langheimer <langheimer@gmail.com>
2019-07-09 15:44:15 +0200

    import eos transit module

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import { core, CoreModule } from './modules/core'
+import { eosTransit, EosTransitModule } from './modules/eosTransit'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
-    core
+    core,
+    eosTransit
   },
   strict: process.env.NODE_ENV !== 'production'
 })
 
 export const vxm = {
-  core: CoreModule.CreateProxy(store, CoreModule) as CoreModule
+  core: CoreModule.CreateProxy(store, CoreModule) as CoreModule,
+  eosTransit: EosTransitModule.CreateProxy(
+    store,
+    EosTransitModule
+  ) as EosTransitModule
 }
