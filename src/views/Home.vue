02921bda8de20fc5abf2d2356d445400c3d8ea3e
Jan Langheimer <langheimer@gmail.com>
2019-07-09 15:44:03 +0200

    testing scatter login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,18 +7,37 @@
       <p>For more information visit website</p>
       <b-button variant="primary" href="#">More Info</b-button>
     </b-jumbotron>
+    <span>Auth: {{ isAuthenticated }}</span> <span @click="logout()">Logout</span>
+    <pre v-if="wallet">{{ wallet.auth }}</pre>
     <HelloWorld :msg="$t('welcome')" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { vxm } from '@/store'
+import HelloWorld from '@/components/HelloWorld.vue'
+import EosTransit from '@/components/authentication/EosTransit.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    EosTransit
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  get wallet() {
+    return vxm.eosTransit.wallet
+  }
+
+  get isAuthenticated() {
+    if (vxm.eosTransit.walletState)
+      return vxm.eosTransit.walletState.authenticated
+    else return false
+  }
+
+  logout() {
+    vxm.eosTransit.logout()
+  }
+}
 </script>
